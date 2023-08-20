8a34884915dd498a7a7ba37e106c15eff63bd15e
Ridwan-Lawal <lawal6382@gmail.com>
2023-08-20 12:02:37 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.js", "./index.html"],
+  theme: {
+    fontFamily: {
+      poppins: ["Poppins", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        red: "hsl(0, 100%, 74%) ",
+        green: "hsl(154, 59%, 51%)",
+        blue: "hsl(248, 32%, 49%)",
+        darkBlue: "hsl(249, 10%, 26%) ",
+        grayishBlue: "hsl(246, 25%, 77%)",
+      },
+    },
+  },
+  plugins: [],
+};
